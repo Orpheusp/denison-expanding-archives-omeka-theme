16f94c0b45954e80dbae1985673eba4c1861d553
Orpheus Peng <stannolite@gmail.com>
2015-10-11 01:40:16 -0400

    added css queue files to header.php

--- common/header.php
+++ common/header.php
@@ -22,44 +22,42 @@
   <!-- Plugin Stuff -->
 
   <!-- Stylesheets -->
-  
   <?php 
-    queue_css_file(array('screen','style'));
+    queue_css_file('screen', 'style'); 
     queue_css_url('//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css');
     echo head_css();
   ?>
-
+  
   <!-- JavaScripts -->
   
 </head>
   
 <?php echo body_tag(); ?>
 
-<input type="checkbox" id="nav-drawer-toggle" class="checkbox-hidden">
-<label class="nav-drawer-shade" for="nav-drawer-toggle" onclick></label>
-<div class="nav-drawer">
-  <div class="nav-container">
+  <input type="checkbox" id="nav-drawer-toggle" class="checkbox-hidden">
+  <label class="nav-drawer-shade" for="nav-drawer-toggle" onclick></label>
+  <div class="nav-drawer">
+    <div class="nav-container">
+      <div class="container">
+        <nav id="primary-nav" role="navigation" class="col-md-12">
+          <?php echo public_nav_main(array('role' => 'navigation', 'class' => 'col-md-6 col-md-offset-1')); ?>
+          
+          <div id="search-container" role="search" class="col-md-4">
+            <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
+              <?php echo search_form(array('show_advanced' => true)); ?>
+            <?php else: ?>
+              <?php echo search_form(); ?>
+            <?php endif; ?>
+          </div>
+        </nav>
+      </div><!-- end of container-->
+    </div><!-- end of nav-container-->
+    
     <div class="container">
-      <nav id="primary-nav" role="navigation" class="col-md-12">
-        <?php echo public_nav_main(array('role' => 'navigation', 'class' => 'col-md-6 col-md-offset-1')); ?>
-
-        <div id="search-container" role="search" class="col-md-4">
-          <?php if (get_theme_option('use_advanced_search') === null || get_theme_option('use_advanced_search')): ?>
-            <?php echo search_form(array('show_advanced' => true)); ?>
-          <?php else: ?>
-            <?php echo search_form(); ?>
-          <?php endif; ?>
-        </div>
-      </nav>
-      
+      <label class="navigation-button col-md-2 col-md-offset-5" for="nav-drawer-toggle">
+        <p class="sans-serif-800">EXPANDING</p>
+        <p class="serif-400">ARCHIVE</p>
+      </label>
     </div><!-- end of container-->
-  </div><!-- end of nav-container-->
-
-  <div class="container">
-    <label class="navigation-button col-md-2 col-md-offset-5" for="nav-drawer-toggle">
-      <p class="sans-serif-800">EXPANDING</p>
-      <p class="serif-400">ARCHIVE</p>
-    </label>
-  </div><!-- end of container-->
-
-</div><!-- end of nav-drawer-->
+    
+  </div><!-- end of nav-drawer-->
