72ffaa09660d97fa0a0bd29b3ebe6e82bb329660
Orpheus Peng <stannolite@gmail.com>
2015-10-13 12:39:06 -0400

    Added skeleton for items advanced search

---
+++ items/search.php
@@ -0,0 +1,14 @@
+<?php
+  $pageTitle = __('Search Items');
+  echo head(array('title' => $pageTitle,
+                  'bodyclass' => 'items advanced-search'));
+?>
+
+<h1><?php echo $pageTitle; ?></h1>
+
+<?php 
+  echo $this->partial('items/search-form.php',
+                      array('formAttributes' => array('id'=>'advanced-search-form'))); 
+?>
+
+<?php echo foot(); ?>
