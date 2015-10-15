e0ef64fb55de2d1f2dc3da041518bdda5826b1b5
Orpheus Peng <stannolite@gmail.com>
2015-10-14 23:13:28 -0400

    removed unused variables.

--- index.php
+++ index.php
@@ -1,34 +1,6 @@
 <?php echo head(); ?>
 
 <?php
-  ($jumbotronSection1BackgroundImage = get_theme_option('homepage_jumbotron_section_1_background_image'));
-  ($jumbotronSection1TitleIcon = get_theme_option('homepage_jumbotron_section_1_title_icon'));
-  ($jumbotronSection1Title1 = get_theme_option('homepage_jumbotron_section_1_title_1'));
-  ($jumbotronSection1Title2 = get_theme_option('homepage_jumbotron_section_1_title_2'));
-  ($jumbotronSection1Info = get_theme_option('homepage_jumbotron_section_1_info'));
-  ($jumbotronSection1Link = get_theme_option('homepage_jumbotron_section_1_link'));
-
-  ($jumbotronSection2BackgroundImage = get_theme_option('homepage_jumbotron_section_2_background_image'));
-  ($jumbotronSection2TitleIcon = get_theme_option('homepage_jumbotron_section_2_title_icon'));
-  ($jumbotronSection2Title1 = get_theme_option('homepage_jumbotron_section_2_title_1'));
-  ($jumbotronSection2Title2 = get_theme_option('homepage_jumbotron_section_2_title_2'));
-  ($jumbotronSection2Info = get_theme_option('homepage_jumbotron_section_2_info'));
-  ($jumbotronSection2Link = get_theme_option('homepage_jumbotron_section_2_link'));
-
-  ($jumbotronSection3BackgroundImage = get_theme_option('homepage_jumbotron_section_3_background_image'));
-  ($jumbotronSection3TitleIcon = get_theme_option('homepage_jumbotron_section_3_title_icon'));
-  ($jumbotronSection3Title1 = get_theme_option('homepage_jumbotron_section_3_title_1'));
-  ($jumbotronSection3Title2 = get_theme_option('homepage_jumbotron_section_3_title_2'));
-  ($jumbotronSection3Info = get_theme_option('homepage_jumbotron_section_3_info'));
-  ($jumbotronSection3Link = get_theme_option('homepage_jumbotron_section_3_link'));
-
-  $jumbotronSection4BackgroundImage = get_theme_option('homepage_jumbotron_section_4_background_image');
-  $jumbotronSection4TitleIcon = get_theme_option('homepage_jumbotron_section_4_title_icon');
-  $jumbotronSection4Title1 = get_theme_option('homepage_jumbotron_section_4_title_1');
-  $jumbotronSection4Title2 = get_theme_option('homepage_jumbotron_section_4_title_2');
-  $jumbotronSection4Info = get_theme_option('homepage_jumbotron_section_4_info');
-  $jumbotronSection4Link = get_theme_option('homepage_jumbotron_section_4_link');
-
   function showSection($sectionNum) {
     $themeUploadsPath = '/files/theme_uploads/';
     $themeOptionPrefix = 'homepage_jumbotron_section_'.$sectionNum.'_';
