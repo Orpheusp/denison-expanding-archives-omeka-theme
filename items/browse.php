af1dc6a0533517f359f8297fd0e1f4ed8641f829
Orpheus Peng <stannolite@gmail.com>
2015-10-16 01:45:06 -0400

    Added links to item for .exhibit-item element.

--- items/browse.php
+++ items/browse.php
@@ -33,17 +33,17 @@
 <div class="search-results">
   <?php foreach (loop('items') as $item): ?>
     <?php 
-      $itemLink = link_to_item();
+      $itemLink = record_url(get_current_record('item'));
       $itemImageTag = item_image('square_thumbnail');
       $itemTitle = metadata('item', array('Dublin Core', 'Title'));
       $itemDescription = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250));
     ?>
-    <div class="exhibit-item">
+    <a class="exhibit-item" href="<?php echo $itemLink ?>">
       <?php echo $itemImageTag ?>
       <h1><?php echo $itemTitle ?></h1>
       <p><?php echo $itemDescription ?></p>
       <?php fire_plugin_hook('public_items_browse_each', array('view' => $this, 'item' =>$item)); ?>
-    </div>
+    </a>
   <?php endforeach; ?>
 </div><!-- end of grid-->
 
