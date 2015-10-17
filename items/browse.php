ef1101daef76cfb35168942c694a800c0276d716
Orpheus Peng <stannolite@gmail.com>
2015-10-17 00:09:17 -0400

    added search panel in items/browse.php

--- items/browse.php
+++ items/browse.php
@@ -4,14 +4,18 @@
 ?>
 
 <div class="container">
-  
   <div class="section-header col-md-10 col-md-offset-1">
     <small>-BROWSE-</small>
     <h1>Items <?php echo __('(%s total)', $total_results); ?></h1>
-    
-  </div><!-- end of section-header -->
-  
-  
+  </div>
+</div>
+
+<?php 
+  echo $this->partial('items/search-form.php',
+                      array('formAttributes' => array('id'=>'advanced-search-form'))); 
+?>
+
+<div class="container">
   <div class="sort-links col-md-8 col-md-offset-2">
     <span class="sort-label">-SORT BY-</span>
     <?php 
@@ -29,11 +33,6 @@
     <span class="outputs-label">-OUTPUT-</span>
     <?php echo output_format_list(false, ''); ?>
   </div><!-- end of outputs -->
-  
-  <div class="item-search-filters col-md-8 col-md-offset-2">
-    <span class="filters-label">-SEARCH FILTERS-</span>
-    <?php echo item_search_filters(); ?>
-  </div><!-- end of item_search_filters -->
   
   <!--<nav class="items-nav navigation secondary-nav">
     <?php /* echo public_nav_items(); */ ?>
@@ -62,7 +61,7 @@
 <div class="container">
   <?php echo pagination_links(); ?>
 </div>
-
+  
 <?php 
   fire_plugin_hook('public_items_browse', array('items'=>$items, 'view' => $this)); 
   echo foot();
