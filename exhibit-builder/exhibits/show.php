7c5c06939017586b2ec5d3876250c851290481a0
Orpheus Peng <stannolite@gmail.com>
2015-10-17 21:07:34 -0400

    reconstruct div structure with the one used commonly in this theme

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -1,33 +1,48 @@
 <?php
-echo head(array(
-    'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
-    'bodyclass' => 'exhibits show'));
+  echo head(array(
+      'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
+      'bodyclass' => 'exhibits show'));
 ?>
 
-<h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></h1>
+<?php
+  $title = metadata('exhibit_page', 'title');
+?>
 
-<div id="exhibit-blocks">
-<?php exhibit_builder_render_exhibit_page(); ?>
-</div>
-
-<div id="exhibit-page-navigation">
-    <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
-    <div id="exhibit-nav-prev">
-    <?php echo $prevLink; ?>
+<div class="container exhibit">
+  <div class="section-header col-md-10 col-md-offset-1">
+    <small>-EXHIBIT-</small>
+    <h1><?php echo $title ?></h1>
+  </div><!-- end of section-header -->
+  
+  <article>
+    <div class="col-md-8 col-md-offset-2">
+      <div class="article-content">
+        <?php exhibit_builder_render_exhibit_page(); ?>
+      </div>
     </div>
+  </article>
+
+  <div id="exhibit-page-navigation">
+    <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
+      <div id="exhibit-nav-prev">
+        <?php echo $prevLink; ?>
+      </div>
     <?php endif; ?>
     <?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
-    <div id="exhibit-nav-next">
-    <?php echo $nextLink; ?>
-    </div>
+      <div id="exhibit-nav-next">
+        <?php echo $nextLink; ?>
+      </div>
     <?php endif; ?>
     <div id="exhibit-nav-up">
-    <?php echo exhibit_builder_page_trail(); ?>
+      <?php echo exhibit_builder_page_trail(); ?>
     </div>
-</div>
+  </div>
 
-<nav id="exhibit-pages">
+  <nav id="exhibit-pages">
     <h4><?php echo exhibit_builder_link_to_exhibit($exhibit); ?></h4>
     <?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
-</nav>
+  </nav>
+  
+</div><!-- end of container -->
+  
 <?php echo foot(); ?>
