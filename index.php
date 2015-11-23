7661cc6988796067c2797caec1326cac82de0f8c
Orpheus Peng <stannolite@gmail.com>
2015-11-23 16:30:25 -0500

    Added a new section on index.php.

--- index.php
+++ index.php
@@ -14,41 +14,41 @@
     $sectionLink = get_theme_option($themeOptionPrefix.'link');
   
   
-    echo __('<div class="section">');
+    echo __("<div class=\"section\"> \n");
     if ($sectionBackgroundImagePath != $themeUploadsPath)
-      echo __('  <img src="'.$sectionBackgroundImagePath.'" class="background">');
+      echo __("  <img src=\"".$sectionBackgroundImagePath."\" class=\"background\"> \n");
     else
-      echo __('  <div class="background"></div>');
-    echo __('  <div class="front">');
-    echo __('    <div class="container">');
-    echo __('      <span class="glyphicon '.$sectionTitleIcon.'" aria-hidden="true"></span>');
-    echo __('      <p class="sans-serif-800">'.$sectionTitle1.'</p>');
-    echo __('      <p class="serif-400">'.$sectionTitle2.'</p>');
-    echo __('    </div>');
-    echo __('  </div>');
-    echo __('  <div class="back">');
-    echo __('    <p>'.$sectionInfo.'</p>');
-    echo __('    <a href="'.$sectionLink.'">LEARN MORE</a>');
-    echo __('  </div>');
-    echo __('</div>');
+      echo __("  <div class=\"background\"></div>\n");
+    echo __("  <div class=\"front\">\n");
+    echo __("    <div class=\"container\">\n");
+    echo __("      <span class=\"glyphicon".$sectionTitleIcon."\" aria-hidden=\"true\"></span>\n");
+    echo __("      <p class=\"sans-serif-800\">".$sectionTitle1."</p>\n");
+    echo __("      <p class=\"serif-400\">".$sectionTitle2."</p>\n");
+    echo __("    </div>\n");
+    echo __("  </div>\n");
+    echo __("  <div class=\"back\">\n");
+    echo __("    <p>".$sectionInfo."</p>\n");
+    echo __("    <a href=\"".$sectionLink."\">LEARN MORE</a>\n");
+    echo __("  </div>\n");
+    echo __("</div>\n");
   }
 ?>
 
 <div class="container-fluid highlight-jumbotron">
-  <div class="col-md-6">
+  <div class="col-md-4">
     <?php showSection(1) ?>
     <?php showSection(2) ?>
   </div>
   
-  <div class="col-md-6">
+  <div class="col-md-4 homepage-logo">
+    <p class="sans-serif-800">EXPANDING</p>
+    <p class="serif-400">ARCHIVE</p>
+  </div>
+  
+  <div class="col-md-4">
     <?php showSection(3) ?>
     <?php showSection(4) ?>
   </div>
-</div>
-
-<div class="homepage-logo">
-  <p class="sans-serif-800">EXPANDING</p>
-  <p class="serif-400">ARCHIVE</p>
 </div>
 
 <?php echo foot(array('displayFooter' => false)); ?>
