24dc091024c713269108aaff598124eaf1ed15cb
Orpheus Peng <stannolite@gmail.com>
2015-11-23 22:57:48 -0500

    refactored index.php.

--- index.php
+++ index.php
@@ -1,38 +1,29 @@
-<?php echo head(); ?>
-
-<?php
-  function showSection($sectionNum) {
-    $themeUploadsPath = '/files/theme_uploads/';
-    $themeOptionPrefix = 'homepage_jumbotron_section_'.$sectionNum.'_';
+<?php 
+  echo head();
+  function themeOptions($optionName) {
+    $themeOptionPrefix1 = 'homepage_jumbotron_section_1_';
+    $themeOptionPrefix2 = 'homepage_jumbotron_section_2_';
+    $themeOptionPrefix3 = 'homepage_jumbotron_section_3_';
+    $themeOptionPrefix4 = 'homepage_jumbotron_section_4_';
     
-    $sectionBackgroundImage = get_theme_option($themeOptionPrefix.'background_image');
-    $sectionBackgroundImagePath = $themeUploadsPath.$sectionBackgroundImage;
-    $sectionTitleIcon = get_theme_option($themeOptionPrefix.'title_icon');
-    $sectionTitle1 = get_theme_option($themeOptionPrefix.'title_1');
-    $sectionTitle2 = get_theme_option($themeOptionPrefix.'title_2');
-    $sectionInfo = get_theme_option($themeOptionPrefix.'info');
-    $sectionLink = get_theme_option($themeOptionPrefix.'link');
-  
-  
-    echo __("<div class=\"section\"> \n");
-    if ($sectionBackgroundImagePath != $themeUploadsPath)
-      echo __("  <img src=\"".$sectionBackgroundImagePath."\" class=\"background\"> \n");
-    else
-      echo __("  <div class=\"background\"></div>\n");
-    echo __("  <div class=\"front\">\n");
-    echo __("    <div class=\"container\">\n");
-    echo __("      <span class=\"glyphicon ".$sectionTitleIcon."\" aria-hidden=\"true\"></span>\n");
-    echo __("      <p class=\"sans-serif-800\">".$sectionTitle1."</p>\n");
-    echo __("      <p class=\"serif-400\">".$sectionTitle2."</p>\n");
-    echo __("    </div>\n");
-    echo __("  </div>\n");
-    echo __("  <div class=\"back\">\n");
-    echo __("    <p>".$sectionInfo."</p>\n");
-    echo __("    <a href=\"".$sectionLink."\">LEARN MORE</a>\n");
-    echo __("  </div>\n");
-    echo __("</div>\n");
+    return array(
+      get_theme_option($themeOptionPrefix1.$optionName),
+      get_theme_option($themeOptionPrefix2.$optionName),
+      get_theme_option($themeOptionPrefix3.$optionName),
+      get_theme_option($themeOptionPrefix4.$optionName)
+    );
   }
 
+  $themeUploadsPath = '/files/theme_uploads/';
+  $sectionBackgroundImages = themeOptions('background_image');
+  $sectionTitleIcons = themeOptions('title_icon');
+  $sectionTitle1s = themeOptions('title_1');
+  $sectionTitle2s = themeOptions('title_2');
+  $sectionInfos = themeOptions('info');
+  $sectionLinks = themeOptions('link');
+?>
+
+<?php
   function showIntro() {
     $themeOption = 'homepage_introduction_content';
     $introContent = get_theme_option($themeOption);
@@ -52,13 +43,97 @@
   </div>
   
   <div class="col-lg-4 col-lg-pull-4 col-md-6 col-sm-12">
-    <?php showSection(1) ?>
-    <?php showSection(2) ?>
+    <!-- section 1 -->
+    <div class="section">
+      <?php if ($sectionBackgroundImages[0] != ""): ?>
+        <img src="<?php echo $themeUploadsPath.$sectionBackgroundImages[0] ?>" class="background">
+      <?php else: ?>
+        <div class="background"></div>
+      <?php endif ?>
+      
+      <div class="front">
+        <div class="container">
+          <span class="glyphicon <?php echo $sectionTitleIcons[0] ?>" aria-hidden="true"></span>
+          <p class="sans-serif-800"><?php echo $sectionTitle1s[0] ?></p>
+          <p class="serif-400"><?php echo $sectionTitle2s[0] ?></p>
+        </div>
+      </div>
+
+      <div class="back">
+        <p><?php echo $sectionInfos[0] ?></p>
+        <a href="<?php echo $sectionLinks[0] ?>">LEARN MORE</a>
+      </div>
+    </div>
+    
+    <!-- section 2 -->
+    <div class="section">
+    <?php if ($sectionBackgroundImages[1] != ""): ?>
+      <img src="<?php echo $themeUploadsPath.$sectionBackgroundImages[1] ?>" class="background">
+    <?php else: ?>
+      <div class="background"></div>
+    <?php endif ?>
+
+      <div class="front">
+        <div class="container">
+          <span class="glyphicon <?php echo $sectionTitleIcons[1] ?>" aria-hidden="true"></span>
+          <p class="sans-serif-800"><?php echo $sectionTitle1s[1] ?></p>
+          <p class="serif-400"><?php echo $sectionTitle2s[1] ?></p>
+        </div>
+      </div>
+
+      <div class="back">
+        <p><?php echo $sectionInfos[1] ?></p>
+        <a href="<?php echo $sectionLinks[1] ?>">LEARN MORE</a>
+      </div>
+    </div>
+    
   </div>  
   
   <div class="col-lg-4 col-md-6 col-sm-12">
-    <?php showSection(3) ?>
-    <?php showSection(4) ?>
+    <!-- section 3 -->
+    <div class="section">
+    <?php if ($sectionBackgroundImages[2] != ""): ?>
+      <img src="<?php echo $themeUploadsPath.$sectionBackgroundImages[2] ?>" class="background">
+    <?php else: ?>
+      <div class="background"></div>
+    <?php endif ?>
+
+      <div class="front">
+        <div class="container">
+          <span class="glyphicon <?php echo $sectionTitleIcons[2] ?>" aria-hidden="true"></span>
+          <p class="sans-serif-800"><?php echo $sectionTitle1s[2] ?></p>
+          <p class="serif-400"><?php echo $sectionTitle2s[2] ?></p>
+        </div>
+      </div>
+
+      <div class="back">
+        <p><?php echo $sectionInfos[2] ?></p>
+        <a href="<?php echo $sectionLinks[2] ?>">LEARN MORE</a>
+      </div>
+    </div>
+    
+    <!-- section 4 -->
+    <div class="section">
+    <?php if ($sectionBackgroundImages[3] != ""): ?>
+      <img src="<?php echo $themeUploadsPath.$sectionBackgroundImages[3] ?>" class="background">
+    <?php else: ?>
+      <div class="background"></div>
+    <?php endif ?>
+
+      <div class="front">
+        <div class="container">
+          <span class="glyphicon <?php echo $sectionTitleIcons[3] ?>" aria-hidden="true"></span>
+          <p class="sans-serif-800"><?php echo $sectionTitle1s[3] ?></p>
+          <p class="serif-400"><?php echo $sectionTitle2s[3] ?></p>
+        </div>
+      </div>
+
+      <div class="back">
+        <p><?php echo $sectionInfos[3] ?></p>
+        <a href="<?php echo $sectionLinks[3] ?>">LEARN MORE</a>
+      </div>
+    </div>
+
   </div>
 </div>
 
